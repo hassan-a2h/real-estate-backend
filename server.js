d2d69257bf99ec29232af803972ffb8e1a7ed443
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-05-30 17:47:23 +0500

    create npm script for auto-reload using nodemon. Create a temp route to test frontend connection

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Test endpoint is working' });
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
