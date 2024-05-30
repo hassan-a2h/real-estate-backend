34f1062a75d4c46ee7fdf409abee1caef6addba5
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-05-30 17:30:18 +0500

    create bare-basic server with essential middlewares

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//  Middlewares used
+app.use(cors());
+app.use(express.json());
+
+//  Initializing connection to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+//  Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
