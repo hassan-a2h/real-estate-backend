84878e9f503ce62c443cd85b34fea80bdabe3bf1
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-05-31 18:48:53 +0500

    create User model, add functions for user signup, login and logout

---
+++ controllers/authController.js
@@ -0,0 +1,57 @@
+import asyncHandler from 'express-async-handler';
+import User from '../models/User.js';
+import generateToken from '../utils/generateToken.js';
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && user.password === password) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie('token', '', { httpOnly: true, expires: new Date(0) });
+  res.json({ message: 'Logged out successfully' });
+});
+
+export { registerUser, authUser, logoutUser };
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import dbConnection from './db/config';
+import dbConnection from './db/config.js';
 
 dotenv.config();
 const app = express();
