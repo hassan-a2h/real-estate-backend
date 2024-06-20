c415c0a226463b2362a08521c7803b40e046dec2
hassan <asd@MacBook-Air-von-asd.local>
2024-06-20 13:50:30 +0500

    update the PORT of the server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import socialsRoutes from './routes/socialRoutes.js';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 //  Middlewares used
 app.use(cors());
