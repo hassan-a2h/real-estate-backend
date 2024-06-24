a99da9979fd4703dd94a251ded441641dd74c1cb
hassan <asd@MacBook-Air-von-asd.local>
2024-06-24 14:05:52 +0500

    create route for getting all agents

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { registerUser, authUser, logoutUser, checkAuth } from '../controllers/authController.js';
+import { registerUser, authUser, logoutUser, checkAuth, getAgents } from '../controllers/authController.js';
 
 router.post('/register', registerUser);
 router.post('/login', authUser);
 router.post('/logout', logoutUser);
 router.get('/check-auth', checkAuth);
+router.get('/agents', getAgents);
 
 export default router;
