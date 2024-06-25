62be5ec6574390f5837320ca2a9f97cf7e0372ea
hassan <asd@MacBook-Air-von-asd.local>
2024-06-25 14:00:01 +0500

    include isRead and receiver fields in order to implement unread feature

--- models/Message.js
+++ models/Message.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 const messageSchema = new mongoose.Schema({
   chatId: { type: mongoose.Schema.Types.ObjectId, ref: 'Chat', required: true },
   senderId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  message: { type: String, required: true },
+  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  isRead: { type: Boolean, default: false },
+  message: { type: String, required: true, maxLength: 512 },
   isPropertyTitle: { type: Boolean, default: false }
 }, { timestamps: true });
 
