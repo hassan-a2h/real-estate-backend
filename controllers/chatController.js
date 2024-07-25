b2babbf418bb466e1e0d36a6f85cff6c14bab0c1
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2024-07-25 13:17:33 +0500

    update: getChat route, create a chat and emit it if new, otherwise return chat will messages

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -29,6 +29,50 @@ export const getChat = async (req, res) => {
         isPropertyTitle: true
       });
       await initialMessage.save();
+
+      const newChat =  await Chat.aggregate([
+        { $match: { _id: chat._id } },
+        {
+          $lookup: {
+            from: 'messages',
+            localField: '_id',
+            foreignField: 'chatId',
+            as: 'messages'
+          }
+        },
+        {
+          $unwind: {
+            path: '$messages',
+            preserveNullAndEmptyArrays: true
+          }
+        },
+        {
+          $sort: {
+            'messages.createdAt': -1
+          }
+        },
+        {
+          $group: {
+            _id: '$_id',
+            chat: { $first: '$$ROOT' },
+            lastMessage: { $first: '$messages' }
+          }
+        },
+        {
+          $replaceRoot: {
+            newRoot: {
+              $mergeObjects: ['$chat', { lastMessage: '$lastMessage' }]
+            }
+          }
+        },
+        {
+          $sort: {
+            'lastMessage.createdAt': -1
+          }
+        }
+      ]);
+
+      io.emit('newChatCreated', { chat: newChat })
     } else if (!chat.listingIds.includes(listingId)) {
       chat.listingIds.push(listingId);
       await chat.save();
@@ -150,6 +194,7 @@ export const createMessage = async (req, res) => {
 
     // Emit the new message to all connected clients
     io.emit('receiveMessage', newMessage);
+    console.log('recevie message event emitted:', newMessage);
 
     res.status(200).json(newMessage);
   } catch (error) {
@@ -180,7 +225,36 @@ export const getUnreadCount = async (req, res) => {
       isRead: false
     });
 
+    const chatsWithLastMessage = await Chat.aggregate([
+      { $match: {
+        $or: [
+          { userId: userId },
+          { agentId: userId }
+        ]
+      } },
+      {
+        $lookup: {
+          from: 'messages',
+          let: { chatId: '$_id' },
+          pipeline: [
+            { $match: { $expr: { $eq: ['$chatId', '$$chatId'] } } },
+            { $sort: { createdAt: -1 } },
+            { $limit: 1 }
+          ],
+          as: 'lastMessage'
+        }
+      },
+      {
+        $addFields: {
+          lastMessage: { $arrayElemAt: ['$lastMessage', 0] }
+        }
+      }
+    ]);
+
+    console.log('chats controller, chats with LastMessages:', chatsWithLastMessage);
+
     const unreadChats = {};
+    const chatLastMessage = {};
 
     for (let message of unreadMessages) {
       if (!unreadChats[message.chatId]) {
@@ -188,10 +262,17 @@ export const getUnreadCount = async (req, res) => {
       } else {
         unreadChats[message.chatId]++;
       }
+
+      if (!chatLastMessage[message.chatId]) {
+        chatLastMessage[message.chatId] = message.message;
+      } else {
+        chatLastMessage[message.chatId] = message.message;
+      }
     }
 
-    res.status(200).json({ unreadCount: Object.keys(unreadChats).length, unreadChats });
+    res.status(200).json({ unreadCount: Object.keys(unreadChats).length, unreadChats, chatLastMessage });
   } catch (error) {
+    console.log('chats controller, error:', error);
     res.status(500).json({ error: error.message });
   }
 };
